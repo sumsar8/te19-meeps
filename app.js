6bc1dc20002138a4a5f4b49f7db38ffb918ff955
sumsar8 <rasmus.oberg@elev.ga.ntig.se>
2022-03-18 10:43:56 +0100

    Appstart allt fungerar

---
+++ app.js
@@ -0,0 +1,26 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const nunjucks = require("nunjucks");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+nunjucks.configure("views", {
+    autoescape: true,
+    express: app
+});
+// view engine setup
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+module.exports = app;
